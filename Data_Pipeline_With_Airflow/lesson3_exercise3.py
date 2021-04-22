0f9b3ce7315251080d84dc65c95937e2eb5280ba
ay123 <keepfashion88@gmail.com>
2021-04-21 23:27:12 -0700

    Exercise

--- Data_Pipeline_With_Airflow/lesson3_exercise3.py
+++ Data_Pipeline_With_Airflow/lesson3_exercise3.py
@@ -55,23 +55,6 @@ stations_subdag_task = SubDagOperator(
     dag=dag,
 )
 
-#
-# TODO: Consolidate check_trips and check_stations into a single check in the subdag
-#       as we did with the create and copy in the demo
-#
-#check_trips = HasRowsOperator(
-#    task_id="check_trips_data",
-#    dag=dag,
-#    redshift_conn_id="redshift",
-#    table="trips"
-#)
-
-#check_stations = HasRowsOperator(
-#    task_id="check_stations_data",
-#    dag=dag,
-#    redshift_conn_id="redshift",
-#    table="stations"
-#)
 
 location_traffic_task = PostgresOperator(
     task_id="calculate_location_traffic",
@@ -79,14 +62,6 @@ location_traffic_task = PostgresOperator(
     postgres_conn_id="redshift",
     sql=sql_statements.LOCATION_TRAFFIC_SQL
 )
-
-#
-# TODO: Reorder the Graph once you have moved the checks
-#
-#trips_subdag_task >> check_trips
-#stations_subdag_task >> check_stations
-#check_stations >> location_traffic_task
-#check_trips >> location_traffic_task
 
 
 trips_subdag_task >> location_traffic_task
