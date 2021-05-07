d0a81d66145894cc8bf92dfd74e6c0a3c7557017
ay123 <keepfashion88@gmail.com>
2021-05-06 23:48:23 -0700

    Data Lake Project

--- Data_Lake/etl.py
+++ Data_Lake/etl.py
@@ -174,7 +174,7 @@ def process_log_data(spark, input_data, output_data):
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "s3a://udacity-dend/output/"
 
     print("Processing song data...")
     process_song_data(spark, input_data, output_data)    
